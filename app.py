f5d404c8d1b61680e588f0b40175003431ce7cb5
moureenakinyi <31238549+moureenakinyi@users.noreply.github.com>
2017-08-27 06:49:46 -0700

    [feature #2133313] add route /morine

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ def hello_world():
 @app.route('/deno')
 def yep():
     return 'we are in deno'
+@app.route('/morine')
+def mo():
+    return 'we are in mo'
 if __name__ == "__main__":
     app.run(debug=true)
